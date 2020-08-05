3f548e35f9b859bec7ae6c2995768d4a24e7030d
buyukkanber <furkanbuyukkanber@outlook.com>
2020-08-05 14:17:43 +0300

    Updates

---
+++ precache-manifest.077b5df3a9c9291b99672718ad04d996.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e10acaaf40e4da2500b7485727dfd92",
+    "url": "/robopals/index.html"
+  },
+  {
+    "revision": "f38e3a49aa32a04de5cb",
+    "url": "/robopals/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3e172a4ba1d6ad9cb5cf",
+    "url": "/robopals/static/css/main.0c188f31.chunk.css"
+  },
+  {
+    "revision": "f38e3a49aa32a04de5cb",
+    "url": "/robopals/static/js/2.e9b9a2a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robopals/static/js/2.e9b9a2a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e172a4ba1d6ad9cb5cf",
+    "url": "/robopals/static/js/main.2b01f970.chunk.js"
+  },
+  {
+    "revision": "19519cacba38688e02b0",
+    "url": "/robopals/static/js/runtime-main.fe99a691.js"
+  },
+  {
+    "revision": "5d5259d61ba7dba0ba622f2841137a17",
+    "url": "/robopals/static/media/AmericanStencil.5d5259d6.woff"
+  }
+]);
